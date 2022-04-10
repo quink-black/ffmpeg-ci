dc37499b9bb0f4fdd148d67901f62d855cb2114e
zhilizhao <zhilizhao@tencent.com>
2022-04-10 22:50:30 +0800

    set cflags -fPIC

--- env.sh
+++ env.sh
@@ -12,3 +12,5 @@ export PKG_CONFIG_PATH=${install_dir}/lib64/pkgconfig:$PKG_CONFIG_PATH
 
 export LD_LIBRARY_PATH="${install_dir}/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
 export PKG_CONFIG_PATH=${install_dir}/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+
+export CFLAGS='-fPIC'
