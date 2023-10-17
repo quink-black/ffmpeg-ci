c28c7daa050878a136d1476057563792746123ba
Zhao Zhili <zhilizhao@tencent.com>
2023-10-17 16:10:46 +0800

    env.sh: fix build on Windows

--- env.sh
+++ env.sh
@@ -5,12 +5,16 @@ export build_dir=${DIR}/build
 export PATH=${install_dir}/bin:$PATH
 export meson_bin=${DIR}/meson/meson.py
 
-export LD_LIBRARY_PATH="${install_dir}/lib"
-export PKG_CONFIG_PATH=${install_dir}/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH="${install_dir}/lib:${install_dir}/lib64:${LD_LIBRARY_PATH}"
+export PKG_CONFIG_PATH="${install_dir}/lib/pkgconfig:${install_dir}/lib64/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-export LD_LIBRARY_PATH="${install_dir}/lib64:${LD_LIBRARY_PATH}"
-export PKG_CONFIG_PATH=${install_dir}/lib64/pkgconfig:$PKG_CONFIG_PATH
-
+if which clang++; then
+    export CC=clang
+    export CXX=clang++
+else
+    export CC=gcc
+    export CXX=g++
+fi
 export CFLAGS='-fPIC'
 export CXXFLAGS='-fPIC'
 export MACOSX_DEPLOYMENT_TARGET=11.0
